9fb476fb7b5998a7c8fbd52b585e0266358a6d9a
emauser1 <emauser1@mci.ext>
2025-09-24 12:56:39 +0300

    set up: react and created basic components

---
+++ firstChallenge/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+export default function Header() {
+  return (
+  <header style={{
+    width:'100%',
+    height:'200px',
+    backgroundColor:'teal',
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    fontSize:'30px',
+    color:'white',
+    boxShadow:'15px,15px,13px,black'
+   }}>
+    <h1>Welcome, Elnakhlawhy Web</h1>
+   </header>
+  )
+}
+
